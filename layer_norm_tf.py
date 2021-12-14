88d67d9fd0dfeafa40f7d5abb4d7b6fe68294577
Kaixi Hou <kaixih@nvidia.com>
2021-12-13 18:18:38 -0800

    Remove old files

--- layer_norm_tf.py
+++ layer_norm_tf.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import time
 
 def layer_norm_tf(x, gamma, beta, epsilon):
   x = tf.convert_to_tensor(x)
@@ -24,4 +25,44 @@ def layer_norm_tf(x, gamma, beta, epsilon):
   assert dy_is_one.numpy() == True
   return y, dgamma, dbeta, dx
 
+def benchmark_tf(input_shape):
+  assert len(input_shape) == 2
+  warmup = 10
+  repeat = 100
+
+  layer = tf.keras.layers.LayerNormalization(axis=(1,))
+
+  def train_step(x):
+    with tf.GradientTape() as tape:
+      tape.watch(x)
+      y = layer(x)
+      loss = tf.reduce_sum(y)
+    dx, (dgamma, dbeta) = tape.gradient(loss, [x, layer.variables])
+    return dx, dgamma, dbeta
+
+  data = tf.random.normal(input_shape)
+
+  for i in range(warmup):
+    dx, dgamma, dbeta = train_step(data)
+  _ = tf.reduce_sum(dx).numpy()
+
+  start = time.time()
+  for i in range(repeat):
+    dx, dgamma, dbeta = train_step(data)
+  _ = tf.reduce_sum(dx).numpy()
+
+  result = time.time() - start
+  print("Time: {:0.2f} ms".format(1000 * result / repeat))
+
+#input_shapes = [
+#    (10, 10000000),
+#    (100, 1000000),
+#    (1000, 100000),
+#    (10000, 10000),
+#    (100000, 1000),
+#    (1000000, 100),
+#    (10000000, 10),
+#  ]
+#for input_shape in input_shapes:
+#  benchmark_tf(input_shape)
 
